e11c7581c75c6aa6e03c6b7f6aa3767008f2f774
Yalcin-Berivan <144229852+Yalcin-Berivan@users.noreply.github.com>
2023-09-28 16:18:30 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+
+//TEMPLETE ENGİNE
+app.set('view engine', 'ejs');
+//MİDDLEWARES
+app.use(express.static('public'));
+//ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('İNDEX SAYFASI');
+  res.status(200).render('index',
+  {page_name:"index"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about',{
+    page_name:"about"
+  });
 });
 const port = 3000;
 app.listen(port, () => {
