f71eeb141687a5a81066de92409188a56f9fffe8
Yalcin-Berivan <144229852+Yalcin-Berivan@users.noreply.github.com>
2023-09-28 13:11:20 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('İNDEX SAYFASI');
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
